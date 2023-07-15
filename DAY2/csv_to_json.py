d9853a73a9a3fa6ce7f8a2f1e84c99f901c6b1bd
ragulrathnat <113578372+ragulrathnat@users.noreply.github.com>
2023-07-15 14:10:31 +0530

    DAY 2 CSVfile handling and logging demonstration

---
+++ DAY2/csv_to_json.py
@@ -0,0 +1,30 @@
+"Program to read a CSV file, convert it to JSON, and save it in another file."
+
+import csv
+import json
+
+filename = input("enter the filename")
+with open(filename,'w',newline='') as fh:
+    w = csv.writer(fh)
+    w.writerow(['NAME','ROLLno','MARK','AVG'])
+
+    while True:
+        name = input("enter the name of students")
+        rollno = int(input("enter the students rollno "))
+        mark = int(input("enter the students mark"))
+        avg = mark//5
+        w.writerow([name,rollno,mark,avg])
+
+        option  = input("enter the yes to continue and no to break")
+        if option.lower() == 'no':
+            break
+try:
+    with open(filename,'r') as fh1:
+        read = csv.DictReader(fh1)
+        read = list(read)
+        jsonfile = input("enter the json filename")
+        with open(jsonfile,'w') as fh2:
+            gett = json.dumps(read,indent = 0)
+            fh2.write(gett)
+except Exception as error:
+    print(error)
---
+++ DAY2/logging_demo_file_save.py
@@ -0,0 +1,42 @@
+"Program to demonstrate the logging functionalit"
+
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+f = logging.Formatter('%(asctime)s-%(levelname)s-%(message)s')
+fh = logging.FileHandler("debugging.log",mode='w')
+fh.setFormatter(f)
+logger.addHandler(fh)
+
+def cheackname(name):
+    logger.debug(f'checking name "{name}" ...')
+    if os.path.exists('data.txt'):
+        with open('data.txt','r') as file1:
+            for i in file1:
+                if i.lower().startswith(f'name: {name.lower()}'):
+                    logger.error(f'Name:"{name}" already exists')
+                    return False
+            if len(name) == 0 :
+                logger.critical("name cannot be blank")
+                return False
+            elif not name.isalpha():
+                logger.critical("name must be alphabet")
+                return False
+            else:
+                logger.error(f'check ssuccessfully')
+                return True
+    else:
+        logger.debug(" no data found")
+        return True
+    
+def savedata(name,age,email):
+    logger.debug("saving details of {name}...")
+    with open('data.txt','a') as file2:
+        file2.write(f'Name: {name} - Age: {age} - Email: {email}\n')
+        logger.info(f'Details saved successfully')
+        print("save successfully")
+
+
+
