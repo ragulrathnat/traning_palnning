5df45b14e8cb49e178909433dcb93fab5fd7a778
ragulrathnat <113578372+ragulrathnat@users.noreply.github.com>
2023-07-15 14:14:25 +0530

    Python PDF File Handling and PEP8 code standard

---
+++ DAY6/pep_code_standard.py
@@ -0,0 +1,24 @@
+
+'''  PEP8 code standard '''
+def maxProduct(array, n):  #function for find the max product in subarray
+    product = array[0]  
+
+    for i in range(n):  
+        prod = array[i]  
+
+        for j in range(i + 1, n):  
+            product = max(product, prod)  
+            prod *= array[j]  
+
+        product = max(product, prod)  
+  
+    return product  
+
+nums = int(input("enter the array size"))
+
+array = []
+
+for i in range(nums):
+    array.append(int(input("enter the value")))
+
+print("The maximum product of subarrays is:", maxProduct(array, nums))  
